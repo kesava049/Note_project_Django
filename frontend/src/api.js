5ca915ea73e5589faf48eebf68d238cf7fab3716
kalepallikesavulareddy@gmail.com <kalepallikesavulareddy@gmail.com>
2025-06-16 13:19:09 +0530

    added apiUrl

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
-const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-})
+
+const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
+
+const api = await axios.get(apiUrl/{baseURL});
+
+// const api = axios.create({
+//     baseURL: import.meta.env.VITE_API_URL
+// })
 
 api.interceptors.request.use(
     (config) => {
---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+window.configs = {
+    apiUrl: '/choreo-apis/noteprojectdjango/backend/v1',
+};
