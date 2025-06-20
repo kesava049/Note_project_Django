6ec2c8f0c7af7c80428605ff0fe0c8fa44be46e1
kalepallikesavulareddy@gmail.com <kalepallikesavulareddy@gmail.com>
2025-06-20 20:58:06 +0530

    changed local db

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,11 +4,11 @@ import { ACCESS_TOKEN } from './constants'
 
 const apiUrl = window?.configs?.apiUrl ? window.configs.apiUrl : "/";
 
-const api = await axios.get(apiUrl/{baseURL});
+// const api = await axios.get(apiUrl/{baseURL});
 
-// const api = axios.create({
-//     baseURL: import.meta.env.VITE_API_URL
-// })
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
 
 api.interceptors.request.use(
     (config) => {
